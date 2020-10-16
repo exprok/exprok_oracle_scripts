55d5a928378da8b9431433443ac4e3aea2f356d7
exprok <exprok@inbox.ru>
2020-10-17 01:21:24 +0300

    first without sigma

--- `сигма/определение организации.sql
+++
@@ -1,8 +0,0 @@
-select distinct case terra_name
-             when 'Тихвин 187605' then 'ПСК'
-             when 'Санкт-Петербург_16' then 'ПЭС'
-             when '120 Омская область' then 'ОЭК'
-       end Org
-
-from c_terra
-where terra_name in('Тихвин 187605','Санкт-Петербург_16','120 Омская область')
--- `сигма/убить расчет.sql
+++
@@ -1,1 +0,0 @@
-select 'alter system kill session '''||s.SID||','||s.SERIAL#||''';'   from v$session s where s.ACTION like '%CALC%'
